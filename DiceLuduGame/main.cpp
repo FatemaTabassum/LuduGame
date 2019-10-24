676697c644ebf505f72c3e2908c37364ae37d1c9
Fatema Tabassum <fl19g@my.fsu.edu>
2019-10-24 13:55:04 -0400

    first commit

---
+++ DiceLuduGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DiceLuduGame
+//
+//  Created by Fatema on 10/24/19.
+//  Copyright © 2019 Liza. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
